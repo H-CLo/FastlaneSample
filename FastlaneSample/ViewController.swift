cf0c1270985fdd2aef66dfd4cb4f7ad5545c537e
H-CLo <nba1895@gmail.com>
2018-10-10 15:54:08 +0800

    Add project

---
+++ FastlaneSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneSample
+//
+//  Created by Hung Chang Lo on 2018/10/10.
+//  Copyright © 2018 Hung Chang Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
